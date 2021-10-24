c1f84acd930422aac7b7a70a17ada48e1867c170
bashayerH <Bashayer.Ismaeel.jed.k2@tuwaiq.edu.sa>
2021-10-25 01:55:48 +0300

    done!!

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,28 +2,38 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
 
-   private lateinit var falseButton:Button
-   private lateinit var trueButton:Button
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var falseButton:Button
+    private lateinit var trueButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        falseButton = findViewById(R.id.false_button)
-        trueButton = findViewById(R.id.true_button)
+    falseButton = findViewById(R.id.false_button)
+    trueButton = findViewById(R.id.true_button)
 
-        falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
-        }
 
-        trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
-        }
 
+    falseButton.setOnClickListener {
+        val toast :Toast=  Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP,0,250)
+        toast.show()
     }
+
+    trueButton.setOnClickListener {
+        val toast :Toast= Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP,0,250)
+        toast.show()
+    }
+
+
+}
 }
