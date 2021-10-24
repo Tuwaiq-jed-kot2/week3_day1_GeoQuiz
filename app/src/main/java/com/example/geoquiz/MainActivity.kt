0c96f943bfd9cf7c1313993e4d0ec8afd6f32d31
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-10-24 20:17:50 +0300

    solved

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var falseButton:Button
-   private lateinit var trueButton:Button
+    private lateinit var falseButton: Button
+    private lateinit var trueButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +18,25 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         trueButton = findViewById(R.id.true_button)
 
+        /*
+        i tried this code below to change toast position
+        but i don't know for some reason the toast position does not change
+         */
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG);
+           val toast1 : Toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG);
+            toast1.setGravity(Gravity.TOP,50,50)
+            toast1.show()
+
         }
 
         trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
+
+            val toast1 : Toast = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG);
+            toast1.setGravity(Gravity.TOP, 0, 0)
+            toast1.show()
         }
 
     }
+
 }
