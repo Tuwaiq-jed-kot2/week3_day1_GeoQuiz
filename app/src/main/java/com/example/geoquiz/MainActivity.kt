9f846afc31cda0ba9c8eb6653f7fa4f18ca935d3
shuruqt1000 <92253616+shuruqt1000@users.noreply.github.com>
2021-10-24 17:58:04 +0300

    Create README.md

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 
@@ -18,11 +19,15 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
 
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
+            val toastShow = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG)
+            toastShow.setGravity(Gravity.TOP,0,150)
+            toastShow.show()
         }
 
         trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
+            val toastShow2 = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG)
+            toastShow2.setGravity(Gravity.TOP,0,150)
+            toastShow2.show()
         }
 
     }
