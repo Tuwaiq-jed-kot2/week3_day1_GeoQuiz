3aea1d9f3e8649ceb2c2ef494508901f20497d07
m-ofi <m7mdawfi@gmail.com>
2021-10-24 19:08:27 +0300

    week3_day1_GeoQuiz

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # week3_day1_GeoQuiz
 Part1: 
 In the GeoQuize app:
-1- change the text size 
-2 -text color 
-3- give margin to the buttons
+1- change the text size, New text size is set to 21sp
+2 -text color, New text color is set to purple_700
+3- give margin to the buttons, trueButton right-margin is 10dp, falseButton left margin is 10dp
 ---------------------------------------------
 Part2: 
 Customizing the Toast 
 For this challenge, customize the toast to show at the top instead of the bottom of the screen.
 To change how the toast is displayed, use the Toast class’s setGravity function. Use Gravity.TOP for the gravity value. 
 Refer to the developer documentation at developer.android.com/reference/kotlin/android/widget/Toast#setgravity for more details.
+
+As it said in Android documentation about Toast setGravity method, this method doesn't work anymore for devices
+running API30 or higher.
--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +18,16 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         trueButton = findViewById(R.id.true_button)
 
+//        As it said in Android documentation about Toast setGravity method, this method doesn't work anymore for devices
+//        running API30 or higher.
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
+            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
         }
 
         trueButton.setOnClickListener {
             Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
         }
+
 
     }
 }
