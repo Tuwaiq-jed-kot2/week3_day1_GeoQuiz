43d6f82f4cd6b24d31860892bb7cd94ea8bf0f72
ranaowaidhi <ranaowaidhi@gmail.com>
2021-10-24 20:56:53 +0300

    Peace of cake ;)

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +20,15 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
 
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
+            val toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP,0,200)
+            toast.show()
         }
 
         trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
+            val toast = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP,0,200)
+            toast.show()
         }
 
     }
