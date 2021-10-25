c389205f8dcbec05fe4733b62e67979adb733a45
Bashayr yahya al zahrani <Bashayr.Yahya.jed.k2@tuwaiq.edu.sa>
2021-10-25 08:13:59 +0300

    tried to answer bonus Q , may its wrong

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -7,8 +7,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var falseButton:Button
-   private lateinit var trueButton:Button
+    private lateinit var falseButton: Button
+    private lateinit var trueButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +18,17 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
 
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
-        }
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
+            val myToast1 = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            myToast1.setGravity(Gravity.TOP, 0, 0)
+            myToast1.show()
 
-        trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
-        }
+            trueButton.setOnClickListener {
+                Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
+                val myToast2 = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+                myToast2.setGravity(Gravity.TOP, 0, 0)
+                myToast2.show()
+            }
 
+        }
     }
-}
