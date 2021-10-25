bc6ac810049050785d2b2d53580666d9d6a8b828
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-25 05:42:19 +0300

    Problem with toast display

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,17 +14,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun setGravity(gravity: Int, xOffset: Int, yOffset: Int): Unit{}
 
         falseButton = findViewById(R.id.false_button)
         trueButton = findViewById(R.id.true_button)
 
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_LONG).show()
+            val myToast1=Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
+            myToast1.setGravity(Gravity.TOP,0,0)
+            myToast1.show()
         }
 
         trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG).show()
+            val myToast2=Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT)
+            myToast2.setGravity(Gravity.TOP,0,0)
+            myToast2.show()
         }
-
     }
 }
