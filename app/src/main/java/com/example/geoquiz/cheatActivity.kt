c81b801a6f24bd507d3a12842cb70254ebb5400f
madod <Rawan.hmaid.jed.k2@tuwaiq.edu.sa>
2021-10-29 02:32:40 +0300

    ..

---
+++ app/src/main/java/com/example/geoquiz/cheatActivity.kt
@@ -0,0 +1,32 @@
+package com.example.firstapplicationreplaning
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.geoquiz.R
+
+class cheatActivity : AppCompatActivity() {//1
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+
+}
+
+
+
+
+
+
+
